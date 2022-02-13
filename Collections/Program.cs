7caae19d9e0dba7b264e2a5f1adc180635d3bce1
rumeysadurak <81680325+rumeysadurak@users.noreply.github.com>
2022-02-13 18:07:15 +0300

    13.02.2022- C# Camp

---
+++ Collections/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        //ctrl+k ctrl+c ->>comment
+        //ctrl+k ctrl+u ->>uncomment
+
+        static void Main(string[] args)
+        {
+            //string[] array = new string[] {"Selvi","Sevil","Veli","Engin" };
+            //Console.WriteLine(array[0]);
+            //Console.WriteLine(array[1]);
+            //Console.WriteLine(array[2]);
+            //Console.WriteLine(array[3]);
+            //Console.WriteLine("----------------------\n");
+
+            //Generic yapı <>
+            List<string> myList = new List<string> { "Ayşe"};
+            myList.Add("Selvi");
+            Console.WriteLine(myList[0]);
+            Console.WriteLine(myList[1]);
+            myList.Add("Kelebek");
+            Console.WriteLine(myList[2]);
+            Console.WriteLine(myList[0]);
+
+            Console.WriteLine("----------------------\n");
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager //CRUD operations- create,read,update,delete
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine(product.ProductName+" eklendi.");
+
+        }
+
+        public void Delete(Product product)
+        {
+            Console.WriteLine(product.ProductName + " silindi.");
+
+        }
+
+        public void Update(Product product) {
+
+            Console.WriteLine(product.ProductName + " güncellendi.");
+        }
+
+        
+
+
+    }
+}
